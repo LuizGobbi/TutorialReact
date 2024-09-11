5a4d6aa0a9510cd0f3e4528b1cf1b86766f58135
Luiz Gobbi <luiz@acsn.com.br>
2024-09-11 17:13:05 -0300

    React Tutorial complete

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Open_Sans } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
  
-export const lusitana = Lusitana({
+export const lusitana = Open_Sans({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
